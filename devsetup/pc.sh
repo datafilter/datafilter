223a5999057a0c35a648c2b3d0f66d96b71feef1
datafilter <151140131+datafilter@users.noreply.github.com>
2025-02-19 23:08:16 +0100

    Update pc.sh

--- devsetup/pc.sh
+++ devsetup/pc.sh
@@ -4,7 +4,10 @@
 # sudo /bin/bash -c "$(curl -fsSL https://github.com/datafilter/datafilter/raw/main/devsetup/pc.sh)"
 
 # Install just enough to surf & do basics
-dnf install -y @base-x gnome-shell firefox nautilus gnome-system-monitor gnome-disk-utility gnome-software gedit ptyxis --setopt=install_weak_deps=False
+dnf install -y @base-x gnome-shell firefox nautilus gnome-disk-utility gedit ptyxis
+
+# Optional installs --setopt=install_weak_deps=False
+# dnf install -y gnome-system-monitor gnome-software
 
 # Boot into desktop
 systemctl set-default graphical.target
@@ -30,6 +33,4 @@ systemctl enable crond
 (crontab -l 2>/dev/null; echo "0 2 * * 0 dnf --refresh upgrade -y && dnf autoremove -y && dnf needs-restarting && [ \$? -eq 1 ] && reboot") | crontab 
 
 # Update and reboot
-dnf autoremove -y
-dnf --refresh upgrade -y
 reboot
