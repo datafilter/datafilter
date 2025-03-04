447e03b5b30870706ebaa57ed4be0878eb2d13db
datafilter <151140131+datafilter@users.noreply.github.com>
2025-03-04 17:13:05 +0100

    tidy(docs): Use short script download url

--- pc/setup.sh
+++ pc/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # To install, run:
-# sudo /bin/bash -c "$(curl -fsSL https://github.com/datafilter/datafilter/raw/main/pc/setup.sh)"
+# sudo /bin/bash -c "$(curl -fsSL https://start.datafilter.xyz/pc/setup.sh)"
 
 # UI
 dnf install -y @base-x 
