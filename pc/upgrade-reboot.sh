6c2decf60305b8c6507d627b91edd07e44876fea
datafilter <151140131+datafilter@users.noreply.github.com>
2026-02-17 22:22:47 +0100

    Simplify script by removing redundant 'sudo' commands
    Removed unnecessary 'sudo' from commands since the script is invoked with sudo.

--- pc/upgrade-reboot.sh
+++ pc/upgrade-reboot.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# NOTE sudo implied (called by script already invoked with sudo)
+# so we don't need to add it to chmod, systemclt or curl
+
 # Configuration
 MY_URL="https://start.datafilter.xyz/pc"
 FILES=("upgrade-reboot.service" "upgrade-reboot.timer")
@@ -10,15 +13,15 @@ echo "Installing Weekly Upgrade-Reboot Service..."
 
 # 1. Download the units
 for FILE in "${FILES[@]}"; do
-    sudo curl -sL "$MY_URL/$FILE" -o "$TARGET_DIR/$FILE"
+    curl -sL "$MY_URL/$FILE" -o "$TARGET_DIR/$FILE"
 done
 
 # 2. Set permissions
-sudo chmod 644 $TARGET_DIR/upgrade-reboot.*
+chmod 644 $TARGET_DIR/upgrade-reboot.*
 
 # 3. Reload systemd and enable the timer
-sudo systemctl daemon-reload
-sudo systemctl enable --now upgrade-reboot.timer
+systemctl daemon-reload
+systemctl enable --now upgrade-reboot.timer
 
 echo "-------------------------------------------------------"
 echo "Success! Your system will now:"
