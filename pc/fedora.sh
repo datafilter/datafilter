2efc9f0e9590c10693af6624b7bf6f5c9b4627a4
datafilter <151140131+datafilter@users.noreply.github.com>
2026-02-17 06:56:37 +0100

    Add bootable disk creation instructions to fedora.sh
    Added instructions for creating a bootable external disk from an ISO image.

--- pc/fedora.sh
+++ pc/fedora.sh
@@ -5,6 +5,27 @@
 ### Download Fedora network installer image (Fedora Everything 43 x86_64)
 # https://www.fedoraproject.org/misc/#everything
 
+### Make a bootable external disk from an iso
+# BEFORE plugging in the external disk, run:
+# diskutil list
+# plug in the external list and run diskutil list again to identify the new drive
+# NOTE - the next operations ERASE the drive
+# NOTE - if you pick the wrong one, you'll BRICK your machine.
+# unmount it:
+# diskutil unmountDisk /dev/disk4
+# write the .iso to the disk. Take note of the "r" prefix after "of=.."
+# we refer to /dev/disk4 above, but /dev/rdisk4 in the next command
+# sudo dd if=/Users/your-username/Downloads/Fedora-Everything-netinst-x86_64-43-1.6.iso of=/dev/rdisk4 bs=4m status=progress
+# It might appear to hang but after some time you'll see
+#  1172107616 bytes (1170 MB, 1116 MiB) transferred 272.728s, 4294 kB/s
+#  278+1 records in
+#  278+1 records out
+#  1172107616 bytes transferred in 272.919884 secs (4293171 bytes/sec)
+# finally, run:
+# diskutil eject /dev/disk4
+# that'll print
+# Disk /dev/disk4 ejected
+
 ### Download the script
 # curl -fsSL https://start.datafilter.xyz/pc/fedora.sh > fedora.sh ; chmod +x fedora.sh
 
